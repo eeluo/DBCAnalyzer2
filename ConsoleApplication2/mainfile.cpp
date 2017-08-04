f35f0ae88d44364bef2ea6469751bee6a38d086d
eeluo <1980277695@qq.com>
2017-08-04 13:35:58 +0800

    final

--- ConsoleApplication2/mainfile.cpp
+++ ConsoleApplication2/mainfile.cpp
@@ -5,9 +5,6 @@
 #include <iostream>
 #include <Windows.h>
 
-//#define DEBUG
-#ifndef DEBUG
-
 int main()
 {
 	std::string command_name;
@@ -22,42 +19,5 @@ int main()
 	return EXIT_FAILURE;
 }
 
-#else
 
-#include <gtest\gtest.h>
-#include <gmock\gmock.h>
-#include <deque>
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
-
-TEST(yizhengc, split1)
-{
-	std::deque<std::string> vs;
-	SPILT("he llo wor ld he llo wo rld   ", ' ', vs);
-	EXPECT_EQ(8, vs.size());
-}
-
-TEST(yizhegc, spilt2)
-{
-	std::vector<std::string> vs;
-	std::vector<uint32_t> vi;
-	split_number_and_string("hello 2 3 4 5 sdfgh 45 ty 9", vi, vs);
-	EXPECT_EQ(6, vi.size());
-	EXPECT_EQ(3, vs.size());
-}
-
-TEST(yizhengc, regex)
-{
-	DBCAnalyzer afile;
-	auto descriptor = afile.Analyze("D:\\Git-SourceTree\\test.dbc");
-	EXPECT_EQ(3, descriptor.Messages().size());
-	EXPECT_EQ(6, descriptor.Comments().size());
-	EXPECT_EQ(8, descriptor.AttributeValues().size());
-	EXPECT_EQ(5, descriptor.SignalValues().size());
-}
-#endif
 
